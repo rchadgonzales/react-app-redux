957069066cbcd22d09b259086da0bd13727fc9c7
Richard <rchadgonzales@gmail.com>
2022-09-29 13:58:34 +0800

    Initial commit

--- README.md
+++ README.md
@@ -68,3 +68,31 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+#
+
+#
+
+# Redux Crash Course with ReactJS
+
+This is from the [YouTube crash course](https://www.youtube.com/watch?v=93p3LxR9xfM&t=61s).
+
+This is about what Redux is and build a React app from scratch and add all of the boilerplate for Redux and work with the store/state, actions, reducers and all of the other fundamentals of the Redux state manager.
+
+#
+
+#
+
+# Redux Crash Course
+
+> Code from the "Redux Crash Course With React". Simple implementation of Redux using a couple React components.
+
+## Quick Start
+
+```bash
+# Install dependencies
+npm install
+
+# Serve on localhost:3000
+npm start
+```
---
+++ src/components/Postform.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createPost } from "../actions/postActions";
+
+class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      body: "",
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const post = {
+      title: this.state.title,
+      body: this.state.body,
+    };
+    /*
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(post),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+      */
+    // CALL ACTION
+    this.props.createPost(post);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <label>Title: </label> <br />
+            <input
+              type="text"
+              name="title"
+              onChange={this.onChange}
+              value={this.state.title}
+            />
+          </div>
+          <br />
+          <div>
+            <label>Body: </label> <br />
+            <textarea
+              name="body"
+              cols="30"
+              rows="7"
+              onChange={this.onChange}
+              value={this.state.body}
+            ></textarea>
+          </div>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+PostForm.propTypes = {
+  createPost: PropTypes.func.isRequired,
+};
+
+// export default Postform;
+export default connect(null, { createPost })(PostForm);
